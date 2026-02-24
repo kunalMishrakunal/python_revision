9dd4b53222760b2b55a8cf7f3ec86f266e5424d6
Kunal Mishra <knlkunal242@gmail.com>
2026-02-24 13:32:07 +0000

    py full revision

---
+++ py-basic-re/02_basic.py
@@ -0,0 +1,9 @@
+for i in range(1, 21, 2):
+    print(i) # 1 3 5 7 9 11 13 15 17 19
+    
+    
+a = []
+
+print(b) # []
+for a in range(1, 21):
+    print(a[1: 21: 2]) # 1 3 5 7 9 11 13 15 17 19
--- py-basic-re/test.py
+++ py-basic-re/test.py
@@ -55,3 +55,4 @@ print(reverse("ABCDEF"))
 
 if True:
     print("Your time in near future will be very good")
+    
