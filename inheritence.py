88a38fd69ffba57c39cc92435beb5a6e7390a126
Kunal Mishra <knlkunal242@gmail.com>
2026-07-16 15:07:16 +0000

    revision

---
+++ inheritence.py
@@ -0,0 +1,12 @@
+class Employee:
+    company = "ITC"
+    def show(self):
+        print(f"The name is {self.name} and the salary is {self.salary}")
+        
+class Programmer:
+    def show(self):
+        print(f"The name is {self.name} and the salary is {self.salary}")
+        
+        def showLanguage(self):
+            print(f"The name is {self.name} and he is good with {self.language} language")
+            
